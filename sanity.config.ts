a61c02d8af7c31e16a65166b3efe4416e43ef671
frytom67 <Tom.erik.frydenlund@gmail.com>
2024-06-17 07:54:04 +0200

    modified:   sanity.config.ts 	new file:   schemaTypes/components/DoorsOpenInput.tsx 	modified:   schemaTypes/eventType.ts 	new file:   structure/index.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,17 +2,24 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {structure} from './structure'
 
-export default defineConfig({
+const config = defineConfig
+({
   name: 'default',
   title: 'Day one with Sanity',
 
   projectId: '8ogsechc',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins:
+  [
+    structureTool({structure}), 
+    visionTool(),
+  ],
 
-  schema: {
+  schema: 
+  {
     types: schemaTypes,
   },
 })
